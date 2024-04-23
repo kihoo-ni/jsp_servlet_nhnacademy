39048874e42f9dd243ecaa80442830dc71714763
kihoo-ni <gudcjf532@gmail.com>
2024-04-23 16:29:31 +0900

    second commit

---
+++ hello/src/main/java/com/nhnacademy/hello/MultiServlet.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+public class MultiServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger(MultiServlet.class.getName());
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)  throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+       String[]multi=req.getParameterValues("tool");
+
+       PrintWriter out = resp.getWriter();
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+       for(int i=0;i<multi.length;i++){
+        if(i==multi.length-1){
+            out.print(multi[i]);
+            break;
+        }
+       out.print(multi[i]);
+       out.println(",");
+
+       }
+    }
+
+
+
+}
